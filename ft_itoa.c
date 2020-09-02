4dccacfa43137f0268ac15aeb4855023c2f1e1a1
paul gonzales <paulgonzales@MacBook-Pro.local>
2020-09-03 00:17:59 +0200

    ITOA fix PART 2 finis

--- ft_itoa.c
+++ ft_itoa.c
@@ -6,7 +6,7 @@
 /*   By: gpaul <gpaul@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/09/01 22:42:20 by gpaul             #+#    #+#             */
-/*   Updated: 2020/09/03 00:07:04 by gpaul            ###   ########.fr       */
+/*   Updated: 2020/09/03 00:17:00 by gpaul            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -36,33 +36,29 @@ char	*ft_itoa(int n)
 	char	*re;
 	int		i;
 	int		signe;
+	long	nb;
 
+	nb = n;
 	i = 0;
 	signe = 0;
 	
 	if (!(re = malloc(sizeof(char) * 12)))
 		return (0);
-	if (n == -2147483648)
-	{
-		//re = "-2147483648\0";
-		//re[11] = '\0';
-		return ("-2147483648\0");
-	}
-	if (n == 0)
+	if (nb == 0)
 	{
 		re[0] = '0';
 		re[1] = '\0';
 		return (re);
 	}
-	if (n < 0)
+	if (nb < 0)
 	{
-		n = -n;
+		nb = -nb;
 		signe = 1;
 	}
-	while (n > 0)
+	while (nb > 0)
 	{
-		re[i] = ((n % 10) + '0');
-		n = n / 10;
+		re[i] = ((nb % 10) + '0');
+		nb = nb / 10;
 		i++;
 	}
 	if (signe == 1)
