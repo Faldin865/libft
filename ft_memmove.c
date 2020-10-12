326b3d46cf3abf3a2a1956d9e3b964a5231998a6
paul gonzales <paulgonzales@MacBook-Pro.local>
2020-10-12 19:51:26 +0200

    norme memmove.c

--- ft_memmove.c
+++ ft_memmove.c
@@ -6,32 +6,30 @@
 /*   By: gpaul <gpaul@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/09/03 01:48:36 by gpaul             #+#    #+#             */
-/*   Updated: 2020/10/12 19:14:55 by gpaul            ###   ########.fr       */
+/*   Updated: 2020/10/12 19:50:43 by gpaul            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-void    *ft_memmove(void *destination, const void *source, size_t size)
+void	*ft_memmove(void *destination, const void *source, size_t size)
 {
-    size_t            i;
-    unsigned char    *src;
-    unsigned char    *dst;
+	size_t			i;
+	unsigned char	*src;
+	unsigned char	*dst;
 
-    i = 0;
-    src = (unsigned char *)source;
-    dst = (unsigned char *)destination;
-    if (src < dst)
-    {
-        while (size > 0)
-        {
-            size--;
-            dst[size] = src[size];
-        }
-    }
-    else
-    {
-        ft_memcpy(dst, src, size);
-    }
-    return (dst);
+	i = 0;
+	src = (unsigned char *)source;
+	dst = (unsigned char *)destination;
+	if (src < dst)
+	{
+		while (size > 0)
+		{
+			size--;
+			dst[size] = src[size];
+		}
+	}
+	else
+		ft_memcpy(dst, src, size);
+	return (dst);
 }
